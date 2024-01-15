427600e827858f3a9f7d7a4d3a90f268ea38e826
Simone-Corso <scorso26@gmail.com>
2024-01-15 17:26:50 +0100

    per il momento stampa in modo errato e andrò a realizzare il form per vote

--- hotels.php
+++ hotels.php
@@ -84,7 +84,7 @@ foreach ($hotels as $hotel) {
 
         echo "Nome: " . $hotel['name'] . "<br>";
         echo "Descrizione: " . $hotel['description'] . "<br>";
-        echo "Parcheggio: " . ($hotel['parking'] ? 'true' : 'false'). "<br>";
+        echo "Parcheggio: " . $hotel['parking'] . "<br>";
         echo "Voto: " . $hotel['vote'] . "<br>";
         echo "Distanza dal centro: " . $hotel['distance_to_center'] . "<br>";
     }
