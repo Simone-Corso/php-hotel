5258cb97690b5b6e0383ff8a211193dfe1b3d50a
Simone-Corso <scorso26@gmail.com>
2024-01-15 15:38:50 +0100

    stampa tutto nella pagina.

--- hotels.php
+++ hotels.php
@@ -43,5 +43,15 @@ $hotels = [
 //in questa parte dovrò usare foreach per stamparlo in pagina
 
 foreach ($hotels as $hotel) {
-    echo "Nome: " . $hotel['name'] . "<br>" ;}
+
+    echo "Nome: " . $hotel['name'] . "<br>";
+    echo "Descrizione: " . $hotel['description'] . "<br>";
+    echo "Parcheggio: " . $hotel['parking'] . "<br>";
+    echo "Voto: " . $hotel['vote'] . "<br>";
+    echo "Distanza dal centro: " . $hotel['distance_to_center'] . "<br>";
+
+
+
+
+}
     ?>
