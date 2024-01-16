43ac8e61d57c85fba2796c8b9e6d1c2343dcf894
Simone-Corso <scorso26@gmail.com>
2024-01-16 15:39:14 +0100

    risolvere la parte nel stampare quando l'utente clicca

--- hotels.php
+++ hotels.php
@@ -4,13 +4,13 @@
     <meta charset="UTF-8">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style/style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>php-hotels</title>
 </head>
 <body>
 
-<table class="table table-dark table-hover">
+<table class="table table-red table-hover w-50 mx-auto my-auto">
         <thead>
             <tr>
                 <th>
@@ -59,7 +59,7 @@
         <div class="row">
             <form action="./hotels.php" method="GET">
                 <div class="mb-3 form-check">
-                    <select class="form-select mb-2" aria-label="Default select example" id="vote" name="vote">
+                    <select class="form-select mb-2 w-50 mx-auto my-auto mt-5" aria-label="Default select example" id="vote" name="vote">
                         <option selected value="0">Selezionare un minimo di stelle</option>
                         <option value="1">Una Stella</option>
                         <option value="2">Due Stelle</option>
@@ -68,21 +68,23 @@
                         <option value="5">Cinque Stelle</option>
                     </select>
                 </div>
-                <div class="mb-3 form-check">
-                    <select class="form-select mb-2" aria-label="Default select example" id="parking" name="parking">
+                <div class="mb-3 form-check my-auto mx-auto text-center">
+                    <select class="form-select mb-2 w-50 mx-auto my-auto" aria-label="Default select example" id="parking" name="parking">
                         <option selected value="2">Selezionare il parcheggio</option>
                         <option value="1">Si</option>
                         <option value="0">No</option>
                     </select>
-                    <button type="submit" class="btn btn-primary ms-auto">Invia</button>
+                    <div class="container-btn d-flex justify-content-center">
+                    <button type="submit" class="btn btn-primary">Invia</button>
+                    </div>
                 </div>
             </form>
         </div>
     </div>
 
-    <div class="container">
+    <div class="container d-flex justify-content-center">
         <div class="row">
-            <div class="col-md-12">
+            <div class="col-md-12 my-auto mx-auto text-center">
                 <h1>Risultati della tua ricerca : </h1>
 
                 <!--qua andrò a mettere foreach per stampare quando l'utente andrà a fare la ricerca desiderata!-->
@@ -153,6 +155,7 @@ $hotels = [
 $parkingFilter = (isset($_GET['parking']) && is_numeric($_GET['parking']) && $_GET['parking'] == 1) ? true : false;
 
 
+
 //in questa parte dovrò usare foreach per stamparlo in pagina
 
 $finalHotels = $hotels;
@@ -167,11 +170,8 @@ if (isset($_GET['vote']) && is_numeric($_GET['vote']) && $_GET['vote'] != 0) {
     $finalHotels = array_filter($finalHotels, function ($hotel) {
         return $hotel['vote'] >= $_GET['vote'];
     });
+
 };
-
-
-
-    
 
 ?>
 
